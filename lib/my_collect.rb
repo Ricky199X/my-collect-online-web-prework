eb4529a0deb9704c6192d13b1dfaef6ecdccd95e
Ricky Rojas <rickyrojas19@gmail.com>
2019-03-18 00:55:17 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -8,3 +8,15 @@ def my_collect(array)
   end
   result
 end
+
+
+def my_collect(array)
+  result = [] 
+  i = 0
+  while i < array.length do
+    yield array[i]
+    result << array[i].upcase
+    i += 1
+  end
+  result
+end
