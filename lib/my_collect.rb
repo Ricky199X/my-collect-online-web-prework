3758059f8dc63c93da3b4fd774e1456e381ac5fc
Ricky Rojas <rickyrojas19@gmail.com>
2019-03-18 00:56:27 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -8,15 +8,3 @@ def my_collect(array)
   end
   result
 end
-
-
-def my_collect(array)
-  result = [] 
-  i = 0
-  while i < array.length do
-    yield array[i]
-    result << array[i].split(" ").first
-    i += 1
-  end
-  result
-end
